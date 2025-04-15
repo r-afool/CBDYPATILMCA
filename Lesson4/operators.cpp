4f06c44bcbafd66aaa49554751f38424dff8a840
Rudraksh Jain <rudraksh05jain@gmail.com>
2025-04-15 15:43:00 +0530

    Lesson 4

---
+++ Lesson4/operators.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    bool isFalse = false;
+    int a = 5;
+    // cout<<isFalse<<isFalse++<<endl;
+    cout<<a<<++a<<a<<a<<a++<<a++<<a++<<a;//98997659
+    // cout<<isFalse;
+}
