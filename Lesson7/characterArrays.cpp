8e7d68856022a370bc3206e42820cbd3c9b37117
Rudraksh Jain <rudraksh05jain@gmail.com>
2025-04-18 15:46:22 +0530

    Lesson7

---
+++ Lesson7/characterArrays.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char crr[1000];
+    // cin>>crr;
+    // cout<<crr;
+    // cin.get(crr,1000);
+    //there is a delimter over here with value = /n
+    // cin.get(crr,1000,'$');
+    cin.get(crr,1000,'$');
+    cout<<crr;
+}
---
+++ Lesson7/scope.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+// using namespace std;
+int main(){
+    int cout = 5;
+    std::cout<< (cout<<cout); 
+}
